01f3eaf43f1273380c78af7f21814fdef41c91cf
Anas Aswad <anas@alaan.tv>
2018-05-12 05:05:37 +0400

    fix package name and use jackson instread of javax.json

--- src/main/java/media/dee/dcms/webapp/userprofile/PieItemGetDataCommand.java
+++ src/main/java/media/dee/dcms/webapp/userprofile/PieItemGetDataCommand.java
@@ -1,5 +1,8 @@
 package media.dee.dcms.webapp.userprofile;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import media.dee.dcms.components.WebComponent;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
@@ -7,9 +10,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.log.LogService;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonValue;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -19,6 +19,7 @@ import java.util.concurrent.atomic.AtomicReference;
 @WebComponent.Command.For(value="getData", component=ProfilePieItem.class)
 public class PieItemGetDataCommand implements WebComponent.Command {
     private final AtomicReference<LogService> logRef = new AtomicReference<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Reference
     void setLogService( LogService log ) {
@@ -26,23 +27,21 @@ public class PieItemGetDataCommand implements WebComponent.Command {
     }
 
     @Override
-    public JsonValue execute(JsonValue... command) {
+    public JsonNode execute(JsonNode... command) {
         Bundle bundle = FrameworkUtil.getBundle(this.getClass());
-        URL dataURL = bundle.getResource(String.format("/data/pie/%s.json", ((JsonObject)command[0]).getInt("instanceID")));
+        URL dataURL = bundle.getResource(String.format("/data/pie/%s.json", command[0].get("instanceID").asInt()));
         if( dataURL == null ){
-            return Json.createObjectBuilder()
-                    .add("action","error")
-                    .add("code", "not-fount")
-                    .build();
+            return objectMapper.createObjectNode()
+                    .put("action","error")
+                    .put("code", "not-fount");
         }
         try (InputStream dataInStream = dataURL.openStream()){
-            return Json.createReader(dataInStream).readObject();
+            return objectMapper.readValue(dataInStream, ObjectNode.class);
         } catch (IOException ex){
             logRef.get().log(LogService.LOG_ERROR, "Error Reading data", ex);
-            return Json.createObjectBuilder()
-                    .add("action","error")
-                    .add("code", ex.getMessage())
-                    .build();
+            return objectMapper.createObjectNode()
+                    .put("action","error")
+                    .put("code", ex.getMessage());
         }
     }
 }
